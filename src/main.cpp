b6d6c23327b267d33c85a2c18a87401936c04dd7
alexander.hemberg@gmail.com <alexander.hemberg@gmail.com>
2018-04-29 22:40:57 +0200

    Add basic functionality.
    Reads voltage drop over resistor and sets pwm. Sends values when
    requested.

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,38 @@
 #include <Arduino.h>
+#include <TinyWire.h>
+
+#define ADC1 A3
+#define ADC2 A2
+#define PWM_PIN 1
+#define I2C_ADDRESS 1
+
+int ad_val1 {0}, ad_val2 {0}, pwmval {0};
+
+void onI2CReceive(int howMany){
+	// loops, until all received bytes are read
+	while(TinyWire.available()>0){
+		pwmval = TinyWire.read();
+	}
+}
+
+void onI2CRequest() {
+	TinyWire.send(ad_val1);
+  TinyWire.send(ad_val2);
+  TinyWire.send(pwmval);
+}
 
 void setup() {
-    // put your setup code here, to run once:
+  pinMode(PWM_PIN, OUTPUT);
+  pinMode(ADC1, INPUT);
+  pinMode(ADC2, INPUT);
+
+  TinyWire.begin(I2C_ADDRESS);
+  TinyWire.onReceive(onI2CReceive);
+  TinyWire.onRequest(onI2CRequest);
 }
 
 void loop() {
-    // put your main code here, to run repeatedly:
+  ad_val1 = analogRead(ADC1);
+  ad_val2 = analogRead(ADC2);
+  analogWrite(PWM_PIN, pwmval);
 }

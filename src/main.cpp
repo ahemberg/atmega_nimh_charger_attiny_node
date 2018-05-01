abebf4e42d8b1219f9a8a0e5275393c99fd745a4
ahemberg <UfQRemBWB6fHxXtHV3Id>
2018-05-01 22:25:20 +0200

    Change so that first two bytes of int are sent over I2C

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 #define I2C_ADDRESS 1
 
 int ad_val1 {0}, ad_val2 {0}, pwmval {0};
+float boom = 1337.1337;
 
 void onI2CReceive(int howMany){
 	// loops, until all received bytes are read
@@ -16,8 +17,10 @@ void onI2CReceive(int howMany){
 }
 
 void onI2CRequest() {
-	TinyWire.send(ad_val1);
-  TinyWire.send(ad_val2);
+	TinyWire.send(ad_val1 >> 8 & 0xFF);
+	TinyWire.send(ad_val1 & 0xFF);
+	TinyWire.send(ad_val2 >> 8 & 0xFF);
+  TinyWire.send(ad_val2 & 0xFF);
   TinyWire.send(pwmval);
 }
 
@@ -32,7 +35,8 @@ void setup() {
 }
 
 void loop() {
-  ad_val1 = analogRead(ADC1);
+	analogReference(DEFAULT);
+	ad_val1 = analogRead(ADC1);
   ad_val2 = analogRead(ADC2);
   analogWrite(PWM_PIN, pwmval);
 }
